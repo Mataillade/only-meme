679ba704e08f1b47650a2c2468b4ba52991f04cd
Wylhem <wylhemdorv@gmail.com>
2023-12-04 23:41:40 +0100

    💄 Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary : "#4FD8E0",
+        secondary : {
+          DEFAULT : "#436475",
+          50 : "rgba(42, 163, 239, 0.04)",
+        },
+      }
+    },
+  },
+  plugins: [],
+}
