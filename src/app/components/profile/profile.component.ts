bd11ff7076657a7ad08a40be4e1114d0cb1a4045
mataillade <matias.taillade@etu.u-bordeaux.fr>
2023-12-17 16:06:12 +0100

    profil update on home page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CookieService} from "../../service/cookie/cookie.service";
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  name : string;
 
+  constructor(cookieService: CookieService) {
+    let cookieName = cookieService.getCookie("user");
+    if (cookieName == null) {
+      this.name = "Anonymous";
+    } else {
+      this.name = cookieName;
+    }
+  }
 }
